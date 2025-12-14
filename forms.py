35eae87f416a5818c743747a686101230580162c
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2025-12-14 17:06:51 +0500

    Task 2: Orders

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from .models import Order
 
-class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, max_value=20, initial=1)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
--- urls.py
+++ urls.py
@@ -1,14 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+app_name = 'orders'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls')),
-    path('orders/', include('orders.urls')),
-    path('', include('shop.urls')),
+    path('create/', views.order_create, name='order_create'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
