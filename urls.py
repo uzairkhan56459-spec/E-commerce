866c925f4e84da4bdcd1fd2254ad820afa7c5b44
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-13 03:46:37 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,20 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from shop import views
-from django.contrib.auth.models import User # Superuser banane ke liye
 
-# --- Temporary Admin Creator ---
-try:
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin12345')
-except Exception:
-    pass
-# -------------------------------
+app_name = 'shop'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-    # Agar cart ya orders ki urls hain toh wo bhi yahan include honi chahiye
 ]
