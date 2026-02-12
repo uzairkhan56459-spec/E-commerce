4d8954a8fdbf0403b3c727744a954396d3b5f94f
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-12 23:38:07 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ecommerce.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ecommerce.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
