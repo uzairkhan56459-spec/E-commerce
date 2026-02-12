bc86f8b7a6977af8f036bd96411d59de0b8ad781
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-13 04:28:01 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'shop' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
